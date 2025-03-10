719c7ce7e063f545293cc1b1b1b4dd94c8ab9bfc
mocodesmor <98814268+ibrahimhaji0708@users.noreply.github.com>
2025-03-10 16:50:41 +0530

    error handling, fs::, refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
 use std::env;
+use std::fs;
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).expect("Invalid arguments");
 
-    let query = &args[1];
-    let file_path = &args[2];
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.filename);
 
-    println!("Searhing for Q: {query}");
-    println!("searhing for F: {file_path}");
-    // dbg!(args);
+    let contents = fs::read_to_string(config.filename)
+        .expect("Should have been able to read the file");
+
+    println!("File contents:\n{}", contents);
 }
-
