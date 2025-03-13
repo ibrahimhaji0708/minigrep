4eb0e52356a9a816c4bb0cb46e8bf33d5eb19c2f
Ibrahim <98814268+ibrahimhaji0708@users.noreply.github.com>
2025-03-13 16:59:47 +0530

    TTD, error handling

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
-use std::env;
+use std::{env, fs, process,};
+use std::error::Error;
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough args");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("with text:\n{}", contents);
+    Ok(())
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).expect("Invalid arguments");
 
-    println!("Searching for: {}", config.query);
-    println!("In file: {}", config.filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("problem parsing arg: {}", err);
+        process::exit(1);
+});
 
-    let query = &args[1];
-    let file_path = &args[2];
+    println!("searching for {}", config.query);
+    println!("in file {}", config.filename);
 
-    println!("Searhing for Q: {query}");
-    println!("searhing for F: {file_path}");
-    // dbg!(args);
+    if let Err(e) = run(config) {
+        println!("application error: {}", e);
+        process::exit(1);
+    }
 }
-
