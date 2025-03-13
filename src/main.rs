b734f440be225f90731396cf862663502adb54c5
Ibrahim <98814268+ibrahimhaji0708@users.noreply.github.com>
2025-03-13 15:44:02 +0530

    writing tests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searhing for Q: {query}");
-    println!("searhing for F: {file_path}");
-    // dbg!(args);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    // --snip--
 }
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
+}
