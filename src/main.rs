3ca15abaa39b511db7a1a9f693107364981a4c01
Ibrahim <98814268+ibrahimhaji0708@users.noreply.github.com>
2025-03-16 15:37:30 +0530

    environment variables, err handling

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("problem parsing arg: {}", err);
+        eprintln!("problem parsing arg: {}", err);
         process::exit(1);
 });
 
-    println!("searching for {}", config.query);
-    println!("in file {}", config.filename);
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("application error: {}", e);
+        eprintln!("application error: {}", e);
         process::exit(1);
     }
 }
